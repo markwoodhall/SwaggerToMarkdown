0a2870476b8c4ec7532675555eae241c1a6a06cf
Mark Woodhall <mark.woodhall@gmail.com>
2016-07-27 21:16:14 +0100

    Initial commit.

---
+++ src/SwaggerToMarkdown/SwaggerToMarkdown.fs
@@ -0,0 +1,32 @@
+module App
+
+open Suave
+open Suave.Filters
+open Suave.Operators
+open Suave.Successful
+open Suave.Web
+open FSharp.Data
+
+let generateMarkdown swaggerUrl =
+    match swaggerUrl with
+    | Choice1Of2 s -> sprintf "%s" s
+    | Choice2Of2 s -> "Nothing"
+
+let markdown : WebPart =
+    path "/markdown" >=> choose [
+      GET  >=> request(fun r -> OK <| generateMarkdown (r.queryParam("swaggerUrl")))
+      RequestErrors.NOT_FOUND "Unsupported HTTP Method, did you mean to make a GET request" ]
+
+let app =
+    choose [
+      markdown
+      RequestErrors.NOT_FOUND "Resource not found"
+    ]
+
+[<EntryPoint>]
+let main argv =
+    printfn "%A" argv
+    startWebServer defaultConfig app
+    0
+
+// curl -X GET http://localhost:8083/markdown?swaggerUrl=http%3A%2F%2Fpetstore.swagger.io%2Fv2%2Fswagger.json
